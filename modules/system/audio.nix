ff2bb4ece06bfeab070c7ef8a4ae2d128779f115
Jacky Lin <jacky55668884@gmail.com>
2026-05-16 19:52:05 -0400

    Made nixos bluetooth pairing and auto pairing working

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -2,7 +2,20 @@
 
 {
     hardware.bluetooth.enable = true;
+    hardware.bluetooth.settings = {
+        General = {
+            Enable = "Source,Sink,Media,Socket";
+            AutoEnable = true;
+        };
+    };
     services.blueman.enable = true;
+
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+        alsa.enable = true;
+        wireplumber.enable = true;
+    };
 
     environment.systemPackages = with pkgs; [
         bluez
